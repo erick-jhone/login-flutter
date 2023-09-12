d23809dad3bd895fc428eb88356671d9d255c2d5
ERICK JHONE <erick@sistemaquantum.com.br>
2023-09-12 08:49:50 -0300

    Adição de icone na tela de novo cadastro

--- meu_projeto/lib/form_screen.dart
+++ meu_projeto/lib/form_screen.dart
@@ -54,76 +54,88 @@ class _FormCadastroState extends State<FormCadastro> {
             color: Color.fromARGB(255, 20, 15, 16),
           ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  validator: (value) {
-                    if (valueValidator(value)) {
-                      return 'Insira o usuário';
-                    }
-                    return null;
-                  },
-                  style: const TextStyle(color: Colors.white),
-                  controller: usuarioController,
-                  decoration: const InputDecoration(
-                      hintText: 'Usuário',
-                      hintStyle: TextStyle(color: Colors.white),
-                      fillColor: Color.fromARGB(84, 30, 233, 216),
-                      filled: true),
-                ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 60,),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (valueValidator(value)) {
+                          return 'Insira o usuário';
+                        }
+                        return null;
+                      },
+                      style: const TextStyle(color: Colors.white),
+                      controller: usuarioController,
+                      decoration: const InputDecoration(
+                          hintText: 'Usuário',
+                          hintStyle: TextStyle(color: Colors.white),
+                          fillColor: Color.fromARGB(84, 30, 233, 216),
+                          filled: true),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (senhaCadastroValidator(value)) {
+                          return 'Obrigatorio 8 caracteres, numero e caracter especial';
+                        }
+                        return null;
+                      },
+                      style: const TextStyle(color: Colors.white),
+                      controller: cadastroSenhaController,
+                      obscureText: _isPasswordVisible,
+                      decoration: InputDecoration(
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isPasswordVisible = !_isPasswordVisible;
+                              });
+                            },
+                          ),
+                          hintText: 'Senha',
+                          hintStyle: TextStyle(color: Colors.white),
+                          fillColor: Color.fromARGB(84, 30, 233, 216),
+                          filled: true),
+                    ),
+                  ),
+                  TextButton.icon(
+                      onPressed: () {
+                        if (_cadastroKey.currentState!.validate()) {
+                          Navigator.pop(context);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  backgroundColor:
+                                      Color.fromARGB(255, 0, 129, 0),
+                                  content:
+                                      Text('Cadastro realizado com sucesso'),
+                                  duration: Duration(seconds: 4)));
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                              backgroundColor: Color.fromARGB(255, 255, 0, 0),
+                              content: Text(
+                                  'Usuário repetido ou senha fora dos parâmetros'),
+                              duration: Duration(seconds: 4)));
+                        }
+                      },
+                      icon: const Icon(Icons.add),
+                      label: const Text('Cadastrar'))
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  validator: (value) {
-                    if (senhaCadastroValidator(value)) {
-                      return 'Obrigatorio 8 caracteres, numero e caracter especial';
-                    }
-                    return null;
-                  },
-                  style: const TextStyle(color: Colors.white),
-                  controller: cadastroSenhaController,
-                  obscureText: _isPasswordVisible,
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isPasswordVisible
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
-                      ),
-                      hintText: 'Senha',
-                      hintStyle: TextStyle(color: Colors.white),
-                      fillColor: Color.fromARGB(84, 30, 233, 216),
-                      filled: true),
-                ),
-              ),
-              TextButton.icon(
-                  onPressed: () {
-                    if (_cadastroKey.currentState!.validate()) {
-                      Navigator.pop(context);
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          backgroundColor: Color.fromARGB(255, 0, 129, 0),
-                          content: Text('Cadastro realizado com sucesso'),
-                          duration: Duration(seconds: 4)));
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          backgroundColor: Color.fromARGB(255, 255, 0, 0),
-                          content: Text(
-                              'Usuário repetido ou senha fora dos parâmetros'),
-                          duration: Duration(seconds: 4)));
-                    }
-                  },
-                  icon: const Icon(Icons.add),
-                  label: const Text('Cadastrar'))
             ],
           ),
         ),
